a2a81cec18f9c69c026b68ba994fb2a03017e4f4
Gustavo Pinto <gustavohenrique.86@gmail.com>
2012-02-14 21:45:18 -0300

    correções nas referencias

--- index.php
+++ index.php
@@ -65,6 +65,20 @@ unset($vPublications[0]);
 <style type="text/css" media="all">
   @import "style.css";
 </style>
+ <script type="text/javascript">
+
+   var _gaq = _gaq || [];
+   _gaq.push(['_setAccount', 'UA-465334-5']);
+   _gaq.push(['_trackPageview']);
+
+   (function() {
+     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+   })();
+
+</script>
+
 <script type="text/javascript">
 
 // QuickSearch script for JabRef HTML export 
@@ -537,9 +551,9 @@ typeselect.selectedIndex = 0;
 <body>
 
 <div class="right ribbon-holder"> 
-          <a href="https://github.com/gustavopinto/multitrans" class="red ribbon"> 
-            <span class="text">Fork me on GitHub</span> 
-          </a> 
+  <a href="https://github.com/gustavopinto/multitrans" class="red ribbon"> 
+    <span class="text">Fork me on GitHub</span> 
+  </a> 
 </div> 	
 <div>
 
@@ -600,4 +614,4 @@ typeselect.selectedIndex = 0;
 
 <p class="style11">This template was borred from <a href="http://crestweb.cs.ucl.ac.uk/resources/sbse_repository/repository.html">SBSE Repository</a>. Thanks <a href="http://www.cs.ucl.ac.uk/staff/Yuanyuan.Zhang/">Yuanyuan Zhang</a>.</p>	
 
-</body></html
+</body></html>
